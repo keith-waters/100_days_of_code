06a56341f0e709913ca9b07373e9a2dd47ff546f
Keith Waters <readingwaters@gmail.com>
2021-01-06 22:46:16 -0500

    Add kth smallest in array with quick sort

--- array/sorting.rb
+++ array/sorting.rb
@@ -22,6 +22,81 @@ class Sorting
     end
     max_heap.get_max
   end
+
+  def swap(arr,x,y)
+    temp = arr[x]
+    arr[x] = arr[y]
+    arr[y] = temp
+    arr
+  end
+
+  def partition(arr, left, right)
+    # // Standard partition process of QuickSort().  It considers the last 
+    # // element as pivot and moves all smaller element to left of it 
+    # // and greater elements to right 
+    # int partition(int arr[], int l, int r) 
+    # { 
+    #     int x = arr[r], i = l; 
+    #     for (int j = l; j <= r - 1; j++) { 
+    #         if (arr[j] <= x) { 
+    #             swap(&arr[i], &arr[j]); 
+    #             i++; 
+    #         } 
+    #     } 
+    #     swap(&arr[i], &arr[r]); 
+    #     return i; 
+    # } 
+    x = arr[right]
+    i = left
+    j = left
+    while j <= right-1
+      if arr[j] <= x
+        arr = swap(arr, i, j)
+        i+=1
+      end
+      j+=1
+    end
+    arr = swap(arr, i, right)
+    return {:arr => arr, :pos => i}
+  end
+
+  def quick_sort(arr, k, left, right)
+    # // If k is smaller than number of elements in array 
+    # if (k > 0 && k <= r - l + 1) { 
+    #     // Partition the array around last element and get 
+    #     // position of pivot element in sorted array 
+    #     int pos = partition(arr, l, r); 
+  
+    #     // If position is same as k 
+    #     if (pos - l == k - 1) 
+    #         return arr[pos]; 
+    #     if (pos - l > k - 1) // If position is more, recur for left subarray 
+    #         return kthSmallest(arr, l, pos - 1, k); 
+  
+    #     // Else recur for right subarray 
+    #     return kthSmallest(arr, pos + 1, r, k - pos + l - 1); 
+    # } 
+  
+    # // If k is more than number of elements in array 
+    # return INT_MAX; 
+
+      p arr, k
+    if k > 0 && k <= right-left+1
+      data = partition(arr, left, right)
+      arr = data[:arr]
+      pos = data[:pos]
+      p arr, pos
+
+      return arr[pos] if pos-left == k-1
+      return quick_sort(arr, k, left, pos-1) if pos-left > k-1
+      return quick_sort(arr, k-pos+left-1, pos+1, right)
+    end
+
+  end
+
+  def kth_smallest_quick_sort(arr, k)
+    quick_sort(arr, k, 0, arr.length-1)
+  end
 end
 
 class MinHeap 
--- test/array/sorting_test.rb
+++ test/array/sorting_test.rb
@@ -22,4 +22,10 @@ class TestSorting < Minitest::Test
     assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 4), 10
     assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 6), 20 
   end
+
+  def test_kth_smallest_quick_sort
+    assert_equal @sorting.kth_smallest_quick_sort([4,3,20,7,10,15], 3), 7
+    assert_equal @sorting.kth_smallest_quick_sort([7,10,4,3,20,15], 4), 10
+    assert_equal @sorting.kth_smallest_quick_sort([7,10,4,3,20,15], 6), 20 
+  end
 end
