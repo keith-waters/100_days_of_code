4f47222955795b705cb25d2cfe2d457af2f9c182
Keith Waters <readingwaters@gmail.com>
2021-01-06 22:15:25 -0500

    Review of k'th smallest element min and max heap

--- array/sorting.rb
+++ array/sorting.rb
@@ -5,12 +5,9 @@ class Sorting
 
   def kth_smallest_min_heap(arr, k)
     min_heap = MinHeap.new(arr)
-
-    i = 0
     ans = 0
-    while i < k 
+    for i in (0..k-1)
       ans = min_heap.extract_min
-      i+=1
     end
     ans
   end
@@ -18,65 +15,49 @@ class Sorting
   def kth_smallest_max_heap(arr, k)
     max_heap = MaxHeap.new(arr, k)
 
-    # for (int i = k; i < n; i++) 
-    #   if (arr[i] < mh.getMax()) 
-    #       mh.replaceMax(arr[i]); 
-
-    i = k
-    while i < arr.length
+    for i in (k..arr.length-1)
       if arr[i] < max_heap.get_max
         max_heap.replace_max(arr[i])
       end
-      i+=1
     end
-
     max_heap.get_max
   end
 end
 
 class MinHeap 
-  attr_accessor :heap_size
   attr_accessor :heap_arr
+  attr_accessor :heap_size
 
   def initialize(arr)
-    @heap_size = arr.length
     @heap_arr = arr
-    i = (arr.length-1)/2
-    while i >= 0
+    @heap_size = arr.length
+
+    i = (heap_size-1)/2
+    while i >=0
       min_heapify(i)
       i-=1
     end
   end
 
-  def parent(i)
-    (i-1)/2
-  end
+  def parent(i) (i-1)/2 end
 
-  def left(i)
-    2*i+1
-  end
+  def left(i) 2*i+1 end
 
-  def right(i)
-    2*i
-  end
+  def right(i) 2*i end
 
-  def get_min
-    heap_arr[0]
-  end
+  def get_min() heap_arr[0] end
 
   def extract_min
-    return 0 if @heap_size == 0
-
-    root = heap_arr[0] # store mininum value
-    if @heap_size > 1
+    root = heap_arr[0]
+    if heap_size > 1
       heap_arr[0] = heap_arr[heap_size-1]
-      min_heapify(0)
+      min_heapify(0) 
     end
-    @heap_size-=1
+    self.heap_size-=1
     root
   end
 
-  def swap(x,y)
+  def swap(x, y)
     temp = heap_arr[x]
     heap_arr[x] = heap_arr[y]
     heap_arr[y] = temp
@@ -85,52 +66,36 @@ class MinHeap
   def min_heapify(i)
     left = left(i)
     right = right(i)
-    smallest = i
-    if left < heap_size && heap_arr[left] < heap_arr[i]
-      smallest = left 
-    end
-    if right < heap_size && heap_arr[right] < heap_arr[smallest]
-      smallest = right 
-    end
-    if smallest != i
-      swap(i, smallest) 
-      min_heapify(smallest)
+    min = i
+
+    min = left if left < heap_size && heap_arr[left] < heap_arr[i]
+    min = right if right < heap_size && heap_arr[right] < heap_arr[min]
+    if min != i
+      swap(min, i)
+      min_heapify(min)
     end
   end
 end
 
 class MaxHeap
-  attr_accessor :heap_size
   attr_accessor :heap_arr
+  attr_accessor :heap_size
 
-  def initialize(arr, size)
-    @heap_size = size
+  def initialize(arr, k)
     @heap_arr = arr
+    @heap_size = k
 
     i = (heap_size-1)/2
-    while i>=0
+    while i >= 0
       max_heapify(i)
       i-=1
     end
   end
 
-  def parent(i)
-    (i-1)/2
-  end
-  def left(i)
-    2*i+1
-  end
-  def right(i)
-    2*i+2
-  end
-  def get_max
-    heap_arr[0]
-  end
-  def replace_max(x)
-    heap_arr[0] = x
-    max_heapify(0)
-  end
-
+  def parent(i) (i-1)/2 end
+  def left(i) 2*i+1 end 
+  def right(i) 2*i+2 end
+  def get_max() heap_arr[0] end
 
   def swap(x,y)
     temp = heap_arr[x]
@@ -138,14 +103,20 @@ class MaxHeap
     heap_arr[y] = temp
   end
 
+  def replace_max(x)
+    heap_arr[0] = x
+    max_heapify(0)
+  end
+
   def max_heapify(i)
     left = left(i)
     right = right(i)
     max = i
+
     max = left if left < heap_size && heap_arr[left] > heap_arr[i]
     max = right if right < heap_size && heap_arr[right] > heap_arr[max]
     if max != i
-      swap(i, max)
+      swap(max, i)
       max_heapify(max)
     end
   end
--- test/array/sorting_test.rb
+++ test/array/sorting_test.rb
@@ -18,8 +18,8 @@ class TestSorting < Minitest::Test
   end
 
   def test_kth_smallest_max_heap
-    assert_equal @sorting.kth_smallest_max_heap([4,3,7,10,20,15], 3), 7
-    # assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 4), 10
-    # assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 6), 20 
+    assert_equal @sorting.kth_smallest_max_heap([4,3,20,7,10,15], 3), 7
+    assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 4), 10
+    assert_equal @sorting.kth_smallest_max_heap([7,10,4,3,20,15], 6), 20 
   end
 end
