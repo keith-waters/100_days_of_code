1a1cf3b5890ebb2d560480913b8d585f39397b05
Keith Waters <readingwaters@gmail.com>
2021-01-09 21:41:38 -0500

    Review iterative tower of hanoi

--- stack/tower_of_hanoi.rb
+++ stack/tower_of_hanoi.rb
@@ -8,14 +8,27 @@ class TowerOfHanoi
   end
 
   def iterative(towers)
-    discs = towers[0].length
-    total_moves = (2**discs)-1
+    # For an even number of disks:
+      # make the legal move between pegs A and B (in either direction),
+      # make the legal move between pegs A and C (in either direction),
+      # make the legal move between pegs B and C (in either direction),
+    # repeat until complete.
 
+    # For an odd number of disks:
+      # make the legal move between pegs A and C (in either direction),
+      # make the legal move between pegs A and B (in either direction),
+      # make the legal move between pegs B and C (in either direction),
+    # repeat until complete.
+    # In each case, a total of 2**n − 1 moves are made.
+
+    discs = towers[0].length
     source = towers[0]
     aux = towers[1]
     dest = towers[2]
+    total_moves = (2**discs)-1
 
-    move_disc = lambda do |source, dest| 
+    move_disc = lambda do |source,dest|
+      p source,dest
       if source.length == 0
         source.push(dest.pop)
       elsif dest.length == 0
@@ -27,14 +40,13 @@ class TowerOfHanoi
       end
     end
 
-    i = 1
-    while i <= total_moves
+    for i in (1..total_moves)
+      p i
       move_disc.call(source, dest) if i%3 == 1
-      move_disc.call(source, aux) if i%3 == 2 
+      move_disc.call(source, aux) if i%3 == 2
       move_disc.call(aux, dest) if i%3 == 0
-      i+=1
     end
- 
-    [source, aux, dest]
+
+    discs%2 == 0 ? [source, dest, aux] : [source, aux, dest]
   end
 end
--- test/stack/tower_of_hanoi_test.rb
+++ test/stack/tower_of_hanoi_test.rb
@@ -4,12 +4,13 @@ require_relative "../../stack/tower_of_hanoi"
 class TestTowerOfHanoi < Minitest::Test
   def setup
     @tower_of_hanoi = TowerOfHanoi.new
-    @matrix = [[3,2,1],[], []]
-    @matrix_moved = [[],[], [3,2,1]]
+    @matrix = [[3,2,1],[],[]]
+    @matrix_moved = [[],[],[3,2,1]]
   end
 
   def test_iterative
     assert_equal @tower_of_hanoi.iterative(@matrix), @matrix_moved
+    assert_equal @tower_of_hanoi.iterative([[4,3,2,1],[],[]]), [[],[],[4,3,2,1]]
   end
   
 end
