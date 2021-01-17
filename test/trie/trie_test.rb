990a35cc0fdc26b15fe2e2a15a54da46a24f8719
Keith Waters <readingwaters@gmail.com>
2021-01-16 22:54:30 -0500

    Review trie insert/search, implement trie remove and display

--- test/trie/trie_test.rb
+++ test/trie/trie_test.rb
@@ -8,17 +8,42 @@ class TestTrie < Minitest::Test
   end
 
   def test_trie
-      @trie.insert('the')
-      p @trie.root
+    @trie.insert('the')
+    p @trie.root
   end
 
   def test_search
-      @trie.insert('the')
-      @trie.insert('then')
-      @trie.insert('abba')
-      @trie.insert('borgenburger')
-      assert_equal true, @trie.search('the')
-      assert_equal true, @trie.search('then')
-      assert_equal true, @trie.search('borgenburger')
+    @trie.insert('then')
+    @trie.insert('abba')
+    @trie.insert('borgenburger')
+    assert_equal true, @trie.search('then')
+    assert_equal false, @trie.search('the')
+
+    @trie.insert('the')
+    assert_equal true, @trie.search('the')
+    assert_equal true, @trie.search('borgenburger')
+  end
+
+  def test_remove
+    @trie.insert('then')
+    @trie.insert('abba')
+    assert_equal true, @trie.search('then')
+    assert_equal true, @trie.search('abba')
+
+    @trie.remove('abba')
+    assert_equal false, @trie.search('abba')
+
+    @trie.insert('the')
+    assert_equal true, @trie.search('the')
+    @trie.remove('the')
+    assert_equal false, @trie.search('the')
+    assert_equal true, @trie.search('then')
+  end
+
+  def test_display
+    @trie.insert('the')
+    @trie.insert('abba')
+    @trie.insert('scorcher')
+    assert_equal ['abba','scorcher','the'], @trie.display
   end
 end
--- trie/trie.rb
+++ trie/trie.rb
@@ -1,44 +1,69 @@
 class TrieNode
-  attr_accessor :children
-  attr_accessor :isEndOfWord
+  attr_accessor :children, :end_of_word
   def initialize
-    @children = []
-    26.times do
-      @children.push(nil)
-    end
-    @isEndOfWord = false
+    @children = [].fill(nil, 0, 26)
+    @end_of_word = false
   end
 end
 
 class Trie
-  attr_accessor :root
-  def initialize
-    @root = self.get_node
-  end
-
-  def get_node
-    TrieNode.new
-  end
-
-  def insert(key)
-    temp = @root
-
-    for i in (0..key.length-1)
-      index = key[i].ord - 'a'.ord
-      temp.children[index] = self.get_node if !temp.children[index]
-      temp = temp.children[index]
+    attr_accessor :root
+    def initialize
+      @root = get_node
     end
 
-    temp.isEndOfWord = true
-  end
-
-  def search(key)
-    temp = @root
-    for i in (0..key.length-1)
-      index = key[i].ord - 'a'.ord # 'a'.ord == 97
-      return false if !temp.children[index]
-      temp = temp.children[index]
+    def get_node
+      TrieNode.new
     end
-    !temp.nil? && temp.isEndOfWord
+
+    def insert(key)
+      temp = @root
+      i = 0 
+      while i < key.length
+        index = key[i].ord - 97
+        temp.children[index] = get_node unless temp.children[index]
+        temp = temp.children[index]
+        i+=1
+      end
+      temp.end_of_word = true
+    end
+
+    def search(key)
+      temp = @root
+      key.chars.each_with_index do |c,i|
+        index = key[i].ord - 97
+        return false unless temp.children[index]
+        temp = temp.children[index]
+      end
+      !temp.nil? && temp.end_of_word
+    end
+
+    def remove(key, current=@root, depth=0)
+      return if current.nil?
+
+      if depth == key.chars.length
+        current.end_of_word = false if current.end_of_word
+        current = nil if current.children.empty?
+        return current
+      end
+  
+      index = key[depth].ord - 97
+      current.children[index] = remove(key, current.children[index], depth+1)
+      current = nil if current.children.empty? && current.end_of_word == false
+      current
+    end
+
+  def display(string=[], level=0, current=@root, arr=[])
+    if current.end_of_word
+      arr.push(string.slice(0,level).join)
+    end
+  
+    for i in (0..25)
+      if current.children[i]
+        string[level] = (i + 97).chr
+        display(string, level+1, current.children[i], arr)
+      end
+    end
+    arr
   end
 end
